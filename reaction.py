8573204dd49942bbe62a754d464af554190bb872
ansonfoong <afoong@albany.edu>
2019-05-18 04:55:00 -0400

    Bot adds user to role

--- reaction.py
+++ reaction.py
@@ -11,6 +11,20 @@ async def on_raw_reaction_add(payload):
     if payload.message_id == 533900036917166090:
         print(payload.emoji.name)
         # Find a role corresponding to the Emoji name.
-        guild = payload.guild
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
+
+        role = discord.utils.find(lambda r : r.name == payload.emoji.name, guild.roles)
+
+        if role is not None:
+            print(role.name + " was found!")
+            print(role.id)
+            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
+            await member.add_roles(role)
+            print(member.name)
+
+@client.event
+async def on_ready():
+    print("Bot is ready!")
 
 client.run(TOKEN)
