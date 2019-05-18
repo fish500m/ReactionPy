b695bbfc09303412289039bc79d3e36035bfe2b4
ansonfoong <afoong@albany.edu>
2019-05-18 04:58:07 -0400

    bot removes users from role on reaction remove event

--- reaction.py
+++ reaction.py
@@ -21,7 +21,21 @@ async def on_raw_reaction_add(payload):
             print(role.id)
             member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
             await member.add_roles(role)
-            print(member.name)
+            print("done")
+
+@client.event
+async def on_raw_reaction_remove(payload):
+    if payload.message_id == 533900036917166090:
+        print(payload.emoji.name)
+
+        guild_id = payload.guild_id
+        guild = discord.utils.find(lambda g : g.id == guild_id, client.guilds)
+        role = discord.utils.find(lambda r : r.name == payload.emoji.name, guild.roles)
+
+        if role is not None:
+            member = discord.utils.find(lambda m : m.id == payload.user_id, guild.members)
+            await member.remove_roles(role)
+            print("done")
 
 @client.event
 async def on_ready():
